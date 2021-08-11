c0cf268a622313770bfbbf0462b33045ec947636
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-08-12 02:22:02 +0300

    added script for favicon recreation

---
+++ scripts/create_favicon.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+cd "$DIR"
+
+cd "../src/img"
+
+while read -r icon; do
+  echo "- optimizing icon: $icon"
+  pngcrush -ow -brute "$icon"
+done < <(find "favicon" -type f -name "*.png")
+
+echo "- creating favicon.ico"
+convert "favicon/"*.png "favicon.ico"
